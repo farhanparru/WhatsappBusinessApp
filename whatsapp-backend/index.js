63d1f556a46cf409887ddbbb216bc64e3255b0e8
Shaminfarhan farhan <shaminfarhanfarhan@gmail.com>
2024-10-21 05:02:52 -0500

    CORS

--- whatsapp-backend/index.js
+++ whatsapp-backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'https://chat.invenro.com',
     methods: "GET,POST,PUT,DELETE,PATCH",
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
